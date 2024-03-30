b96131cd01024a2752297064823b3c2bedf566b0
Nicolas Stebner <nicolas_stebner@hotmail.com>
2024-03-30 20:14:10 -0300

    icons-agregados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import "../lib/base.css"
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Component {...pageProps} />
+      <Script src="https://kit.fontawesome.com/e6dba388ef.js"></Script>
     </div>
   )
 }
